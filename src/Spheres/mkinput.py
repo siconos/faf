1d4693d1bb064de30d7c7ba193f5d9d0b1e8a673
Maurice Bremond <Maurice.Bremond@inria.fr>
2014-02-20 17:13:12 +0100

    add spheres falling in a box

---
+++ src/Spheres/mkinput.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+from math import cos, sin, pi
+from numpy.linalg import norm
+
+from Siconos.Mechanics.ContactDetection.Bullet import btQuaternion
+
+from Siconos.Mechanics.ContactDetection.Bullet.BulletWrap import __mul__ as mul
+
+from random import random
+
+theta1 = pi / 2
+a1 = 1
+b1 = 0
+c1 = 0
+n1 = sin(theta1 / 2) / norm((a1, b1, c1))
+
+r1 = btQuaternion(a1 * n1, b1 * n1, c1 * n1, cos(theta1 / 2))
+
+
+def s_v(v):
+    return ' '.join('{0}'.format(iv) for iv in v)
+
+alpha = pi / 2.
+
+with open('input.dat', 'w') as f:
+    # ground
+    f.write('1 -1 0 0  0 -.5 1 0 0 0 0 0 0 0 0 0\n')
+
+    for k in range(0,1):
+
+        for i in range(0,4):
+
+            theta = i * alpha
+            a = 0
+            b = 0
+            c = 1
+            n = sin(theta / 2) / norm((a, b, c))
+
+            r = btQuaternion(a*n, b*n, c*n, cos(theta/2))
+            r = mul(r, r1)
+
+            r.normalize()
+
+            q = (20*cos(i*alpha), 20*sin(i*alpha), 2.5 * k + 5)
+            o = (r.w(), r.x(), r.y(), r.z())
+
+            f.write('2 -2 1 {0} {1} 0 0 0 0 0 0\n'.format(s_v(q),s_v(o)))
+
+    for x in range(0,10):
+        for y in range(0,10):
+            for i in range(0,10):
+
+                theta = 0
+                a = 0
+                b = 0
+                c = 1
+                n = sin(theta / 2) / norm((a, b, c))
+                q1 = (10-2*x+random()/10., 10-2*y+random()/10., i + 5)
+
+                r = btQuaternion(a * n, b * n, c * n, cos(theta / 2))
+                r = mul(r, r1)
+                r.normalize()
+
+                o = (r.w(), r.x(), r.y(), r.z())
+                v1 = (0, 0, 0, 0, 0, 0)
+
+                f.write('0 0 1 {0} {1} {2}\n'.format(s_v(q1), s_v(o), s_v(v1)))
---
+++ src/Spheres/params.py
@@ -0,0 +1,27 @@
+import Siconos.Numerics as Numerics
+
+t0 = 0
+T = 30
+h = 0.0005
+g = 9.81
+theta = 0.50001
+mu = 0.7
+dump_itermax = 80
+dump_probability = .05
+itermax = 100000
+NewtonMaxIter = 20
+tolerance = 1e-8
+solver = Numerics.SICONOS_FRICTION_3D_NSGS
+
+
+fileName = "Spheres"
+title = "Spheres"
+description = """
+Spheres falling on the ground with Bullet collision detection
+Moreau TimeStepping: h={0}, theta = {1}
+One Step non smooth problem: {2}, maxiter={3}, tol={4}
+""".format(h, theta, Numerics.idToName(solver),
+           itermax,
+           tolerance)
+
+mathInfo = ""
