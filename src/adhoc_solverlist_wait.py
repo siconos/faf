12e85bfd31267b924fb4d953cedf37ce4ea3a722
Vincent Acary <vincent.acary@inria.fr>
2017-08-28 15:41:17 +0200

    [src] add a possible adhoc solver list

---
+++ src/adhoc_solverlist_wait.py
@@ -0,0 +1,15 @@
+
+#all_solvers = list(nsgs_series)
+#all_solvers.extend(nsgs_solvers)
+#all_solvers.extend(snsgs_series)
+
+all_solvers=list(nsgs_solvers)
+#all_solvers.remove(nsgs_sfull)
+#all_solvers.remove(snsgs)
+#all_solvers=list(nsn_solvers)
+#all_solvers=list(snsgs_series)
+#all_solvers.extend([nsgs,nsgs_sfull])
+
+#all_solvers=list(VIFixedPointProjection_series)
+#all_solvers.extend(VIExtraGrad_series)
+
