db7d03bd52d8161916af35a259dcf5ccdba26f21
Vincent Acary <vincent.acary@inria.fr>
2017-09-19 09:36:51 +0200

    [scripts] add a name based on date and time when OAR_JOB_ID is not existing

--- scripts/faf-run.sh
+++ scripts/faf-run.sh
@@ -5,13 +5,18 @@ faf_src_dir=$faf_dir/src
 faf_scripts_dir=$faf_dir/scripts
 
 fclib_library_dir=$HOME/fclib-library
+#fclib_library_dir=/scratch/Vincent/fclib-library
 
 comp=$faf_src_dir/comp.py
 .
-
+if test -z ${OAR_JOB_ID}
+then
+example_name=$(date +%F--%T)_${example}_${precision}_${timeout}
+else
 example_name=${OAR_JOB_ID}_${example}_${precision}_${timeout}
-
+fi
 rundir=/nfs_scratch/$USER/faf/$example_name
+#rundir=/scratch/Vincent/faf/$example_name
 mkdir -p $rundir
 
 
@@ -41,6 +46,6 @@ done
 
 cp $faf_scripts_dir/$example/$0 $rundir/$0
 cd ..
-tar zcvf comps-$example_name.tar.gz `find ${example_name} -name comp.hdf5`  ${example_name}/$0
+tar zcvf comps-$example_name.tar.gz `find ${example_name} -name comp.hdf5`  ${example_name}/$0 --force-local
 mkdir -p $faf_dir/results
 mv comps-$example_name.tar.gz $faf_dir/results
